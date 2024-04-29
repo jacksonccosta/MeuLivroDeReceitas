76a9760f156d1a94973da538c0a08fa22a6ea866
Jackson Costa <jackson.costa@papersoft-dms.com>
2024-04-29 20:52:49 -0300

    #393 Criação da classe DataBaseMigration

---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DataBaseMigration.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace MyRecipeBook.Infrastructure;
+
+public class DataBaseMigration
+{
+    public static void Migrate(string connectionString)
+    {
+        EnsureDataBaseCreated(connectionString);
+    }
+
+    private static void EnsureDataBaseCreated(string connectionString)
+    {
+        var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+        var dataBaseName = connectionStringBuilder.InitialCatalog;
+        connectionStringBuilder.Remove("DataBase");
+
+        using var dbConnection = new SqlConnection(connectionStringBuilder.ConnectionString);
+
+        var parameters = new DynamicParameters(connectionStringBuilder);
+        parameters.Add("name", dataBaseName);
+
+        var records = dbConnection.Query("SELECT * FROM sys.databases WHERE name = @name", parameters);
+
+        if (!records.Any())
+            dbConnection.Execute($"CREATE DATABASE {dataBaseName}");
+    }
+}
