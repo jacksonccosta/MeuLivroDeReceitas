cf4898404b61e26f125356a6a7756bdf5719f52e
Jackson Costa <jackson.costa@papersoft-dms.com>
2024-05-08 15:10:32 -0300

    #416 Criar teste de integração para o login

--- tests/WebAPI.Test/CustomWebApplicationFactory.cs
+++ tests/WebAPI.Test/CustomWebApplicationFactory.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using CommonTestUtilities;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain;
 using MyRecipeBook.Infrastructure;
 
 namespace WebAPI.Test;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private User _user = default!;
+    private string _password = string.Empty;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
@@ -25,6 +30,25 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                     options.UseInMemoryDatabase("InMemoryDbForTesting");
                     options.UseInternalServiceProvider(provider);
                 });
+
+                using var scope = services.BuildServiceProvider().CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<MyRecipeBookDbContext>();
+
+                dbContext.Database.EnsureDeleted();
+
+                StartDataBase(dbContext);
             });
+    }
+
+    public string GetName() => _user.Name;
+    public string GetEmail() => _user.Email;
+    public string GetPassword() => _password;
+
+    private void StartDataBase(MyRecipeBookDbContext dbContext)
+    {
+        (_user, _password) = UserBuilder.Build();
+
+        dbContext.Users.Add(_user);
+        dbContext.SaveChanges();
     }
 }
---
+++ tests/WebAPI.Test/Login/DoLoginTest.cs
@@ -0,0 +1,62 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using MyRecipeBook.Communication;
+using MyRecipeBook.Exceptions.ResourcesMessages;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace WebAPI.Test.Login;
+
+public class DoLoginTest(CustomWebApplicationFactory factory) : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly string method = "login";
+    private readonly HttpClient _httpClient = factory.CreateClient();
+
+    private readonly string _name = factory.GetName();
+    private readonly string _email = factory.GetEmail();
+    private readonly string _password = factory.GetPassword();
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = new RequestLoginJson
+        {
+            Email = _email,
+            Password = _password
+        };
+
+        var response = await _httpClient.PostAsJsonAsync(method, request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.GetProperty("name").GetString().Should().NotBeNullOrWhiteSpace().And.Be(_name);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Login_Invalid(string culture)
+    {
+        var request = RequestLoginJsonBuilder.Build();
+
+        if (_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
+            _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
+
+        _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
+
+        var response = await _httpClient.PostAsJsonAsync(method, request);
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage = ResourceMessagesException.ResourceManager.GetString("EMAIL_OR_PASSWORD_INVALID", new CultureInfo(culture));
+        errors.Should().ContainSingle().And.Contain(error => error.GetString()!.Equals(expectedMessage));
+    }
+}
